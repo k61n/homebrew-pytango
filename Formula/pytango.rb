fb5b3aebd2e6dbac4961f2b44dd59c30c6e217f6
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-06-23 11:22:48 +0200

    fix missing boost_python3 lib

--- Formula/pytango.rb
+++ Formula/pytango.rb
@@ -13,16 +13,18 @@ class Pytango < Formula
   depends_on "python" => [:build, :latest]
 
   def install
-    system "curl https://raw.githubusercontent.com/k61n/homebrew-pytango/main/patch/cppzmq.patch " +
-             "-o #{buildpath}/cppzmq.patch"
-    system "git apply cppzmq.patch"
+    system "curl https://raw.githubusercontent.com/k61n/homebrew-pytango/main/patch " +
+             "-o #{buildpath}/patch"
+    system "git apply patch"
     python_exe = "#{HOMEBREW_PREFIX}/bin/python3"
     boost_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix boost`.strip
+    boos_python_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix boost-python3`.strip
     cppzmq_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix cppzmq`.strip
     omni_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix omniorb`.strip
     tango_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix cpptango`.strip
     zmq_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix zeromq`.strip
-    system "BOOST_ROOT=#{boost_root} BOOST_PYTHON_LIB=boost_python311 " +
+    system "BOOST_ROOT=#{boost_root} BOOST_PYTHON_ROOT=#{boos_python_root} " +
+             "BOOST_PYTHON_LIB=boost_python311 " +
              "CPPZMQ_ROOT=#{cppzmq_root} OMNI_ROOT=#{omni_root} " +
              "PKG_CONFIG_PATH=#{tango_root}/lib/pkgconfig TANGO_ROOT=#{tango_root} " +
              "ZMQ_ROOT=#{zmq_root} #{python_exe} setup.py build"
